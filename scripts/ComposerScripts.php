5612ff2a55122a7b5dfcd0be3f4343b4c9f8cd44
Greg Anderson <greg.1.anderson@greenknowe.org>
2022-11-04 15:46:44 -0700

    Remove typehints from file

--- scripts/ComposerScripts.php
+++ scripts/ComposerScripts.php
@@ -10,21 +10,25 @@ namespace PhpSiteRepositoryTool;
 use Composer\Script\Event;
 use Composer\Semver\Comparator;
 use Symfony\Component\Filesystem\Filesystem;
-use Webmozart\PathUtil\Path;
+use Symfony\Component\Finder\Finder;
 
 class ComposerScripts
 {
     public static function configureForPhpVersion(Event $event)
     {
-        $scenario = static::determineScenario();
+        $phpVersion = PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
+
+        $scenario = static::determineScenario($phpVersion);
         $command = "composer scenario $scenario";
         print "> $command\n";
         passthru($command);
+
+        static::removeTypeHints($phpVersion);
     }
 
-    private static function determineScenario()
+    private static function determineScenario($phpVersion)
     {
-        switch (PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION) {
+        switch ($phpVersion) {
             case '5.6':
             case '7.0':
                 return 'php56';
@@ -43,5 +47,29 @@ class ComposerScripts
         }
 
         return 'default';
+    }
+
+    private static function removeTypeHints($phpVersion)
+    {
+        if (version_compare($phpVersion, '7.2') >= 0) {
+            return;
+        }
+
+        $finder = new Finder();
+        $finder->in(dirname(__DIR__) . '/src')->files()->name('*.php');
+
+        foreach ($finder as $file) {
+            print '| ' . $file->getRelativePathname() . PHP_EOL;
+
+            $contents = $file->getContents();
+            $contents = static::removeTypeHintsFromContents($contents);
+
+            file_put_contents($file->getRealPath(), $contents);
+        }
+    }
+
+    private static function removeTypeHintsFromContents($contents)
+    {
+        return preg_replace('#^(\w+)(public|protected|private)(\w+)(array|string|bool)(\w+[^;]+;)#', '${1}${2}${5}', $contents);
     }
 }
